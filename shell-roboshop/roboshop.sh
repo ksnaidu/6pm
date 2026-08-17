4ae350ffd27753acf33ea76a29c9c112911010b0
ksnaidu <ksnaidudba@gmail.com>
2026-08-17 16:33:33 +0530

    robsohop.sh added

--- shell-roboshop/roboshop.sh
+++ shell-roboshop/roboshop.sh
@@ -5,11 +5,12 @@ SG_ID="sg-0629e944a73597de8" # replace with your SG_id
 INSTANCES=("mongodb")
 ZONE_ID="Z06008633JIHZ67B3RC4Q"  # replace with your zone_id
 DOMAIN_NAME="kimidi.site"  # replace with your Domain Name
+SUBNET_ID="subnet-040e46e23d5653bd4"
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do 
-  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0629e944a73597de8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+  INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --subnet-id "$SUBNET_ID" \ --security-group-ids sg-0629e944a73597de8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
   if [ "$instance" != "frontend" ]
   then
      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
