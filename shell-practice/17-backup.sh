9b24ac75be80fadcc3fc50087dd391081a4bb6a0
ksnaidu <ksnaidudba@gmail.com>
2026-08-25 19:23:12 +0530

    17-backup.sh

--- shell-practice/17-backup.sh
+++ shell-practice/17-backup.sh
@@ -61,3 +61,27 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
+if [ ! -z "$FILES" ]
+then
+    echo "Files to zip are: $FILES"
+    TIMESTAMP=$(date +%F-%H-%M-%S)
+    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+
+    if [ -f $ZIP_FILE ]
+    then
+        echo -e "Successfully created Zip file"
+
+        while IFS= read -r filepath
+        do
+            echo "Deleting file: $filepath" | tee -a $LOG_FILE
+            rm -rf $filepath
+        done <<< $FILES
+        echo -e "Log files older than $DAYS from source directory removed ... $G SUCCESS $N"
+    else
+        echo -e "Zip file creation ... $R FAILURE $N"
+        exit 1
+    fi
+else
+    echo -e "No log files found older than 14 days ... $Y SKIPPING $N"
+fi
